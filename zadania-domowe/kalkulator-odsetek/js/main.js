286965f0f3b40b8a243c8f85bb19c6add5d44548
mludkiewicz <mludkiewicz@gmail.com>
2018-01-31 18:27:37 +0100

    init

---
+++ zadania-domowe/kalkulator-odsetek/js/main.js
@@ -0,0 +1,27 @@
+function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek) {
+	if (podatek) {
+		var oprocentowanieOpodatkowane = 0.81*oprocentowanie;
+	} else {
+		oprocentowanieOpodatkowane = oprocentowanie;
+	}
+	
+	var kapital = Math.pow(wplata * (1 + oprocentowanieOpodatkowane/okresKapitalizacji), iloscLat*okresKapitalizacji);
+	return (parseFloat(kapital)).toFixed(2);
+}
+
+function oblicz() {
+
+	var wplata = parseInt(document.getElementById("wplata").value);
+	var iloscLat = parseInt(document.getElementById("ilosc-lat").value);
+	var okresKapitalizacji = parseInt(document.getElementById("okres-kapitalizacji").value);
+	var oprocentowanie = parseInt(document.getElementById("oprocentowanie").value) / 100;
+	var podatek;
+	if (document.getElementById("podatek").checked) {
+		podatek = true;
+	} else {
+		podatek = false;
+	}
+	
+	var wynik = obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek);
+	document.getElementById("wynik").innerHTML = wynik;
+}
